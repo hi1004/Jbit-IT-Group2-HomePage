ccda3f7c14105e0dc5c71419d22c69841263efe9
hi1004 <hi1004@github.com>
2021-11-21 04:32:06 +0900

    add signin images

--- js/register.js
+++ js/register.js
@@ -3,10 +3,13 @@
 const toggle = document.querySelector('.toggle');
 const closeBtn = document.querySelector('.close');
 const container = document.querySelector('.signin-container');
+const imgContainer = document.querySelector('.img-container');
 
 toggle.addEventListener('click', () => {
   container.classList.add('active');
+  imgContainer.classList.add('active');
 });
 closeBtn.addEventListener('click', () => {
   container.classList.remove('active');
+  imgContainer.classList.remove('active');
 });
