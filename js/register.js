a8bae29ae2aed04716af634cacafccb6c57e86ca
hi1004 <hi1004@github.com>
2021-11-21 22:30:47 +0900

    update siginin branch

--- js/register.js
+++ js/register.js
@@ -4,12 +4,18 @@ const toggle = document.querySelector('.toggle');
 const closeBtn = document.querySelector('.close');
 const container = document.querySelector('.signin-container');
 const imgContainer = document.querySelector('.img-container');
+const registerName = document.querySelector('#register-user-name');
+const signinName = document.querySelector('#user-name');
 
 toggle.addEventListener('click', () => {
   container.classList.add('active');
   imgContainer.classList.add('active');
+  setTimeout(() => {
+    registerName.focus();
+  }, 600);
 });
 closeBtn.addEventListener('click', () => {
   container.classList.remove('active');
   imgContainer.classList.remove('active');
+  signinName.focus();
 });
