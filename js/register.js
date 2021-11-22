0be487db524fa17cb708a01ff539ec209e5b95b7
hi1004 <hi1004@github.com>
2021-11-22 20:32:00 +0900

    update register.js

--- js/register.js
+++ js/register.js
@@ -5,15 +5,22 @@ const closeBtn = document.querySelector('.close');
 const container = document.querySelector('.signin-container');
 const imgContainer = document.querySelector('.img-container');
 const registerName = document.querySelector('#register-user-name');
+const registerPassword = document.querySelector('#register-user-password');
+const registerRepeatPassword = document.querySelector('#register-repeat-user-password');
 const signinName = document.querySelector('#user-name');
 
+console.log(registerName);
 toggle.addEventListener('click', () => {
   container.classList.add('active');
   imgContainer.classList.add('active');
   setTimeout(() => {
     registerName.focus();
   }, 600);
+  registerName.value = '';
+  registerPassword.value = '';
+  registerRepeatPassword.value = '';
 });
+
 closeBtn.addEventListener('click', () => {
   container.classList.remove('active');
   imgContainer.classList.remove('active');
